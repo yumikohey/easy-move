9dec5bd83f488c99375a563ad0d08adff3c8e2d2
Xi Huang <xi.huang@morningstar.com>
2017-10-22 00:53:26 -0500

    first question is ready

--- capital-one/src/components/questionnaire/index.js
+++ capital-one/src/components/questionnaire/index.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
-import "../home/home.css";
+import "./questionnaire.css";
 
 class Questionnaire extends Component {
     render() {
         return (
             <div className="container-fluid">
-                <div className="bg-img">
-                    <div className="login-box">
-                        <h1>Hello World</h1>
+                <div className="sf-bg-img"> <div className="bg-layer"></div></div>
+                <div className="main-content">
+                    <div className="questions">
+                        <div className="question-text">
+                            <p>Have you found a new place to move in yet?</p>
+
+                            <div className="response">
+                                <div className="two-option-images">
+                                    <div className="option-image">
+                                        <img src="./img/apartment.png" alt="" width="100px"/>
+
+                                        <div className="option-image-label">
+                                            <p>Yes, I have new address ready.</p>
+                                        </div>
+                                    </div>
+                                    <div className="option-image">
+                                        <img src="./img/apartment.png" alt="" width="100px"/>
+
+                                        <div className="option-image-label">
+                                            <p>No, give me recommendations.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="question-text">
+                            <p>What is your new address?</p>
+
+                            <div className="response">
+                                <input type="text" value=""/>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
