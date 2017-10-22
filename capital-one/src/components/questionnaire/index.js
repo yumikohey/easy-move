9a81c88802240490cc5ea6f78746199cd013c743
Xi Huang <xi.huang@morningstar.com>
2017-10-21 22:58:40 -0500

    setting up second page

---
+++ capital-one/src/components/questionnaire/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import "../home/home.css";
+
+class Questionnaire extends Component {
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="bg-img">
+                    <div className="login-box">
+                        <h1>Hello World</h1>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Questionnaire;
